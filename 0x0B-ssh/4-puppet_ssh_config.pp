6865eaa4aa4a999bad26a9e1e70e6058c650f7ca
juanfe9118 <1168@holbertonschool.com>
2020-03-24 13:01:22 -0500

    Modify file_line name

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Modifies the "ssh_config" file
 
-file_line { 'Modify password authentication':
-    ensure => 'present',
+file_line { 'Turn off passwd auth':
+    ensure => present,
     path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
     match  => 'PasswordAuthentication yes'
 }
 
-file_line { 'Add ssh key file':
-    ensure => 'present',
+file_line { 'Declare identity file':
+    ensure => present,
     path   => '/etc/ssh/ssh_config',
     line   => 'IndentityFile ~/.ssh/holberton'
 }
