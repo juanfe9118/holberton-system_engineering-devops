2f7c115288c746fc64bf9b3a2eddc936070f3324
juanfe9118 <1168@holbertonschool.com>
2020-03-24 12:38:25 -0500

    Modify values of the ensure property

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Modifies the "ssh_config" file
 
 file_line { 'Modify password authentication':
-    ensure => present,
+    ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
     match  => 'PasswordAuthentication yes'
 }
 
 file_line { 'Add ssh key file':
-    ensure => present,
+    ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => 'IndentityFile ~/.ssh/holberton'
 }
