58f1bab22a8221c7f44b3d38d6eecdf7b69f95e4
juanfe9118 <1168@holbertonschool.com>
2020-04-03 22:19:35 -0500

    Modify puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,9 +1,14 @@
-# Puppet script that configures an ubuntu machine to have a cusstom header in the HTTP Response
+# Puppet script that configures an ubuntu machine to have a custom header in the HTTP Response
 
-file_line { 'title':
+file_line { 'Custom Header':
   ensure   => present,
   path     => '/etc/nginx/sites-available/default',
   after    => 'server_name _;',
-  line     => 'add_header X-Served-By \"$HOSTNAME\";',
+  line     => 'add_header X-Served-By "$HOSTNAME";',
   multiple => true
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
