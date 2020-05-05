25c99455feb47087a689fa0c77d621fd844d98a3
juanfe9118 <1168@holbertonschool.com>
2020-05-04 21:21:16 -0500

    Include import guard in code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,29 +4,31 @@ based on the ID given"""
 import requests
 from sys import argv
 
-uid = argv[1]
-all_tasks = 0
-done_tasks = 0
 
-# Get the JSON dictionaries for requested user and all TODOs
-user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                    .format(uid)).json()
-todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+if __name__ == "__main__":
+    uid = argv[1]
+    all_tasks = 0
+    done_tasks = 0
 
-name = user.get("name")
+    # Get the JSON dictionaries for requested user and all TODOs
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(uid)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
-# Get all tasks and completed tasks numbers, and the user completed tasks
-completed_tasks = []
-for task in todos:
-    if int(uid) == task.get('userId'):
-        all_tasks += 1
-        if task.get('completed') is True:
-            done_tasks += 1
-            completed_tasks.append(task)
+    name = user.get("name")
 
-print('Employee {} is done with tasks({}/{}):'.format(name, done_tasks,
-                                                      all_tasks))
+    # Get all tasks and completed tasks numbers, and the user completed tasks
+    completed_tasks = []
+    for task in todos:
+        if int(uid) == task.get('userId'):
+            all_tasks += 1
+            if task.get('completed') is True:
+                done_tasks += 1
+                completed_tasks.append(task)
 
-# Print all completed tasks' titles
-for task in completed_tasks:
-    print('\t {}'.format(task.get('title')))
+    print('Employee {} is done with tasks({}/{}):'.format(name, done_tasks,
+                                                          all_tasks))
+
+    # Print all completed tasks' titles
+    for task in completed_tasks:
+        print('\t {}'.format(task.get('title')))
