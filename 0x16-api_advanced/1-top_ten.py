811cd355ba52d4e12f240bdf52f303a432196d51
juanfe9118 <1168@holbertonschool.com>
2020-05-07 23:23:45 -0500

    Add top 10 function

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,4 +2,20 @@
 """Module that queries the Reddit API and prints the first 10 hot posts listed
 for the subreddit."""
 import requests
-from sys import argv
+
+
+def top_ten(subreddit):
+    """Function that sends a request to the Reddit API and takes a subreddit
+    as parameter, then displays the title of the top ten hot posts.
+
+    subreddit: the subredit to query"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, headers={'user-agent': 'request'},
+                            allow_redirects=False)
+    if response.status_code != requests.codes.ok:
+        print(None)
+        return
+    rdict = response.json()
+    posts = rdict.get('data').get('children')
+    for post in posts:
+        print(post.get('data').get('title'))
