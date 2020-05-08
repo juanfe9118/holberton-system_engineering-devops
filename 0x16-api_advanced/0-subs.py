04a26a93f9abda7de2f85e95b2bd240adb1d6e0e
juanfe9118 <1168@holbertonschool.com>
2020-05-07 23:26:49 -0500

    Modify conditions

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Function that sends a request to the Reddit API and takes a subreddit
-    as parameter, then displays the number of  subscribers of the subreddit
+    as parameter, then displays the number of subscribers of the subreddit.
 
     subreddit: the subredit to query"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers={'user-agent': 'request'},
                             allow_redirects=False)
-    if str(response) != '<Response [200]>':
+    if response.status_code != requests.codes.ok:
         return 0
     rdict = response.json()
     sub_num = rdict.get('data').get('subscribers')
